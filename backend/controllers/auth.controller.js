63fcbe366d7ebe93c276216d9f1788296291bf6d
Getasew Tewachew <getasewtewachew@gmail.com>
2025-12-04 06:05:41 -0800

    jwt fix

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -8,7 +8,7 @@ function generateToken(res,userid){
     res.cookie("token",token,{
         httpOnly:true,
         secure:process.env.NODE_ENV==="production",
-        sameSite:"strict",
+        sameSite:"none",
         maxAge:7*24*60*60*1000,
     });
 
